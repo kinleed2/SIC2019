ea1e617b936bcd3ab8d8c9a2cef7493f614b9916
starrysky-eng <wei5899584@gmail.com>
2019-09-01 19:30:25 +0900

    update

--- project_connet/enemy.h
+++ project_connet/enemy.h
@@ -15,8 +15,10 @@
 void enemy_init();
 void enemy_update();
 void enemy_draw();
-void enemy_guard_move(OBJ2D *obj,int leftLimit, int rightLimit, int speed);
 void enemy_guard_init(OBJ2D *obj, int x, int y, int direction);
+void enemy_guard_move(OBJ2D *obj,int leftLimit, int rightLimit, int speed);
+void enemy_uav_init(OBJ2D *obj, int x, int y, int direction);
 void enemy_uav_move(OBJ2D *obj, int mode, int leftLimit, int rightLimit, int speed, int upLimit, int downLimit);
 void enemy_camera_init(OBJ2D *obj, int x, int y, int direction);
+void enemy_camera_move(OBJ2D *obj, int mode, int leftLimit, int rightLimit, int speed, int upLimit, int downLimit);
 #endif // !INCLUDED_ENEMY
